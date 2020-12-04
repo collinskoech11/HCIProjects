28f6bb03e9d9a0fc2067684347f42fe4ffab6513
Collins Koech <collinskoechck34@gmail.com>
2020-12-04 08:00:16 +0300

    pages update

--- SampleOne/js/App.js
+++ SampleOne/js/App.js
@@ -1,18 +1,55 @@
+/**REsume Services Portfolio */
 document.getElementById('Home').addEventListener('click',
     function() {
         document.querySelector('.HomeCon').style.display = 'block';
         document.querySelector('.ContactCon').style.display = 'none';
         document.querySelector('.AboutCon').style.display = 'none';
+        document.querySelector('.ResumeCon').style.display = 'none';
+        document.querySelector('.ServicesCon').style.display = 'none';
+        document.querySelector('.PortfolioCon').style.display = 'none';
     })
 document.getElementById('Contact').addEventListener('click',
     function(){
         document.querySelector('.HomeCon').style.display = 'none';
         document.querySelector('.ContactCon').style.display = 'block';
         document.querySelector('.AboutCon').style.display = 'none';
+        document.querySelector('.ResumeCon').style.display = 'none';
+        document.querySelector('.ServicesCon').style.display = 'none';
+        document.querySelector('.PortfolioCon').style.display = 'none';
     })
 document.getElementById('About').addEventListener('click',
 function(){
     document.querySelector('.HomeCon').style.display = 'none';
     document.querySelector('.ContactCon').style.display = 'none';
     document.querySelector('.AboutCon').style.display = 'block';
+    document.querySelector('.ResumeCon').style.display = 'none';
+    document.querySelector('.ServicesCon').style.display = 'none';
+    document.querySelector('.PortfolioCon').style.display = 'none';
+})
+document.getElementById('Resume').addEventListener('click',
+function(){
+    document.querySelector('.HomeCon').style.display = 'none';
+    document.querySelector('.ContactCon').style.display = 'none';
+    document.querySelector('.AboutCon').style.display = 'none';
+    document.querySelector('.ResumeCon').style.display = 'block';
+    document.querySelector('.ServicesCon').style.display = 'none';
+    document.querySelector('.PortfolioCon').style.display = 'none';
+})
+document.getElementById('Services').addEventListener('click',
+function(){
+    document.querySelector('.HomeCon').style.display = 'none';
+    document.querySelector('.ContactCon').style.display = 'none';
+    document.querySelector('.AboutCon').style.display = 'none';
+    document.querySelector('.ResumeCon').style.display = 'none';
+    document.querySelector('.ServicesCon').style.display = 'block';
+    document.querySelector('.PortfolioCon').style.display = 'none';
+})
+document.getElementById('Portfolio').addEventListener('click',
+function(){
+    document.querySelector('.HomeCon').style.display = 'none';
+    document.querySelector('.ContactCon').style.display = 'none';
+    document.querySelector('.AboutCon').style.display = 'none';
+    document.querySelector('.ResumeCon').style.display = 'none';
+    document.querySelector('.ServicesCon').style.display = 'none';
+    document.querySelector('.PortfolioCon').style.display = 'block';
 })
