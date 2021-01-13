4f3c1643d835199d280f1ad346aeb4c13f69ab97
BNanyingi <nanyingibarbara@gmail.com>
2021-01-13 16:41:05 +0300

    hosting update

---
+++ SampleTwo/public/js/App.js
@@ -0,0 +1,77 @@
+//toggle dark mode
+document.getElementById("Dmode").addEventListener('click',
+    function(){
+        var container = document.querySelector(".container");
+        container.classList.toggle('dark');
+    })
+//toggle light mode
+    document.getElementById('Lmode').addEventListener('click',
+    function(){
+        var con = document.querySelector('.container');
+        con.classList.remove('dark');
+    })
+
+ //toggle english language homepage
+ document.getElementById('englisho').addEventListener('click',
+function(){
+    var contentEnglish = document.querySelector(".contentEnglish");
+    contentEnglish.classList.toggle("hide");
+});
+
+ document.getElementById('englisho').addEventListener('click',
+function(){
+    var contentKiswahili = document.querySelector(".contentKiswahili");
+    contentKiswahili.classList.toggle("show");
+});
+
+//toggle swahili language  homepage
+document.getElementById('swahili').addEventListener('click',
+function(){
+    var contentKiswahili = document.querySelector(".contentKiswahili");
+    contentKiswahili.classList.toggle("show");
+});
+
+document.getElementById('swahili').addEventListener('click',
+function(){
+    var contentEnglish = document.querySelector(".contentEnglish");
+    contentEnglish.classList.toggle("hide");
+});
+
+
+
+//display features
+document.getElementById('btnSuper').addEventListener('click',
+    function(){
+        var btn = document.querySelector(".btn");
+        btn.style.display ="block";
+    })
+    document.getElementById('btnSuper').addEventListener('click',
+    function(){
+        var bto = document.querySelector("#btnSuper");
+        btn.style.display ="none";
+    })
+
+/*navigation*/
+    function myFunction() {
+        var x = document.getElementById("myTopnav");
+        if (x.className === "topnav") {
+          x.className += " responsive";
+        } else {
+          x.className = "topnav";
+        }
+      }
+
+      /*text to speech*/
+      function textToAudio() {
+        let msg = document.getElementById("text-to-speech").textContent;
+        
+        let speech = new SpeechSynthesisUtterance();
+        speech.lang = "en-US";
+        
+        speech.text = msg;
+        speech.volume = 1;
+        speech.rate = 1;
+        speech.pitch = 1;
+        
+        window.speechSynthesis.speak(speech);
+    }
---
+++ SampleTwo/public/js/about.js
@@ -0,0 +1,48 @@
+document.getElementById("mode").addEventListener('click',
+    function(){
+        var container = document.querySelector(".container");
+        container.classList.toggle('dark');
+    })
+
+// toggle KIswahili content
+document.getElementById('languageKo').addEventListener('click',
+    function(){
+        var eng = document.querySelector('.Englishcontent');
+        eng.style.display= 'none';
+    })
+
+    document.getElementById('languageKo').addEventListener('click',
+    function(){
+        var eng = document.querySelector('.Kiswahilicontent');
+        eng.style.display= 'block';
+    })
+
+
+// toggle english language
+    document.getElementById('LanguageEo').addEventListener('click',
+    function(){
+        var eng = document.querySelector('.Kiswahilicontent');
+        eng.style.display= 'none';
+    })
+
+    document.getElementById('LanguageEo').addEventListener('click',
+    function(){
+        var eng = document.querySelector('.Englishcontent');
+        eng.style.display= 'block';
+    })
+
+
+//toggle dark mode
+
+document.getElementById('Dmode').addEventListener('click',
+    function(){
+        var con = document.querySelector('.modeCon');
+        con.classList.toggle('dark');
+    })
+
+//toggle light mode
+document.getElementById('Lmode').addEventListener('click',
+    function(){
+        var con = document.querySelector('.modeCon');
+        con.classList.remove('dark');
+    })
